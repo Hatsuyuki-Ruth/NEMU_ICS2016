5b342dfbfc6d8552b6780fc7c767ce5de7e09d84
tracer-ics2016 <tracer@njuics.org>
2016-09-05 12:48:46 +0800

    >  compile NEMU 151242041 hatsuyuki Linux PA 4.4.0-36-generic #55-Ubuntu SMP Thu Aug 11 18:00:59 UTC 2016 i686 i686 i686 GNU/Linux  12:48:46 up  1:45,  1 user,  load average: 0.06, 0.05, 0.00 f610c6216677933ba3e6e377564e3297479b39bc

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -38,6 +38,17 @@ static int cmd_q(char *args) {
 
 static int cmd_help(char *args);
 
+static int cmd_si(char *args) {
+//	volatile int arg_len = strlen(args);
+	volatile int arg = 0;
+	while(args != NULL) {
+		arg = arg * 10 + (*args - '0');
+	}
+	if(arg == 0) arg = 1;
+	cpu_exec(arg);
+	return 0;
+}
+
 static struct {
 	char *name;
 	char *description;
@@ -46,7 +57,7 @@ static struct {
 	{ "help", "Display informations about all supported commands", cmd_help },
 	{ "c", "Continue the execution of the program", cmd_c },
 	{ "q", "Exit NEMU", cmd_q },
-
+	{ "si", "Run several steps", cmd_si }
 	/* TODO: Add more commands */
 
 };
