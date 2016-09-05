f8e2c1f75a1cc9119962bf1726c2020584ced2ac
tracer-ics2016 <tracer@njuics.org>
2016-09-05 17:01:02 +0800

    >  compile NEMU 151242041 hatsuyuki Linux PA 4.4.0-36-generic #55-Ubuntu SMP Thu Aug 11 18:00:59 UTC 2016 i686 i686 i686 GNU/Linux  17:01:02 up  5:57,  1 user,  load average: 0.05, 0.04, 0.00 8bdfc84df6902ed260dbcd1890e1e0d8aa782ab3

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -213,8 +213,8 @@ int eval(int p, int q) {
 				cur_level = op_level(tokens[pos].type);
 			}
 		}
-		printf("%d\n", op);
-		switch(tokens[pos].type) {
+		printf("OP: %d\n", op);
+		switch(tokens[op].type) {
 			case '+': return eval(p, op - 1) + eval(op + 1, q);
 			case '-': return eval(p, op - 1) - eval(op + 1, q);	
 			case MUL: return eval(p, op - 1) * eval(op + 1, q);
