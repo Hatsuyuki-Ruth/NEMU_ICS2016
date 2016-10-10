2b614dedddccef9e127ec44ed5801403bad19f9a
tracer-ics2016 <tracer@njuics.org>
2016-10-10 20:12:12 +0800

    >  compile NEMU 151242041 hatsuyuki Linux PA 4.4.0-38-generic #57-Ubuntu SMP Tue Sep 6 15:41:41 UTC 2016 i686 i686 i686 GNU/Linux  20:12:12 up  1:41,  1 user,  load average: 0.00, 0.00, 0.00 1163ae207786ed6c241ee433eb6cc0e862ac90e

--- nemu/src/cpu/exec/push/push_template.h
+++ nemu/src/cpu/exec/push/push_template.h
@@ -6,7 +6,7 @@ make_helper(concat(push_r_, SUFFIX)){
 	int num_reg = instr_fetch(eip, 1) & 7;
 	cpu.esp -= DATA_BYTE;
 	swaddr_write(cpu.esp, DATA_BYTE, REG(num_reg));
-	printf("%d %d\n", num_reg, REG(num_reg));
+	printf("%d %d %d\n", num_reg, REG(num_reg), DATA_BYTE);
 	return 1;
 }
 
