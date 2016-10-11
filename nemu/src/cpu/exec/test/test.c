cd006378a3d2efe76928376c52c4ba0eb9b2d90f
tracer-ics2016 <tracer@njuics.org>
2016-10-11 15:46:44 +0800

    >  compile NEMU 151242041 hatsuyuki Linux PA 4.4.0-38-generic #57-Ubuntu SMP Tue Sep 6 15:41:41 UTC 2016 i686 i686 i686 GNU/Linux  15:46:44 up  1:35,  1 user,  load average: 0.20, 0.06, 0.02 3bac5b04eea59e85f1b792de2a1d17f3fb482984

--- nemu/src/cpu/exec/test/test.c
+++ nemu/src/cpu/exec/test/test.c
@@ -1,11 +1,20 @@
 #include "cpu/helper.h"
 #include "cpu/exec/helper.h"
 
-make_helper(test_r2rm){
-	return 0;
-}
+#define DATA_BYTE 1
+#include "test_template.h"
+#undef DATA_BYTE
 
-make_helper(test_r2rm_b){
-	return 0;
+#define DATA_BYTE 2
+#include "test_template.h"
+#undef DATA_BYTE
+
+#define DATA_BYTE 4
+#include "test_template.h"
+#undef DATA_BYTE
+
+make_helper(test_r2rm){
+	if(ops_decoded.is_operand_size_16) return test_r2rm_w(eip);
+	else return test_r2rm_l(eip);
 }
 
---
+++ nemu/src/cpu/exec/test/test_template.h
@@ -0,0 +1,32 @@
+#include "cpu/exec/helper.h"
+#include "cpu/exec/template-start.h"
+#include "cpu/decode/modrm.h"
+
+make_helper(concat(test_r2rm_, SUFFIX)) {
+	cpu.OF = 0;
+	cpu.CF = 0;
+	ModR_M mod;
+	DATA_TYPE test1, test2, res;
+	Operand op1, op2;
+	int addr_len = 1;
+	mod.val = instr_fetch(eip + 1, 1);
+	if(mod.mod == 0x3) {
+		test1 = REG(mod.R_M);
+		test2 = REG(mod.reg);
+		print_asm("test"str(SUFFIX)" %%%s, %%%s", REG_NAME(mod.R_M), REG_NAME(mod.reg));
+	}
+	else{
+		swaddr_t addr;
+		addr_len = read_ModR_M(eip + 1, &op1, &op2);
+		addr = op2.addr;
+		test1 = MEM_R(addr);
+		test2 = REG(mod.reg);
+		print_asm("test"str(SUFFIX)" %%%s, *0x%x", REG_NAME(mod.reg), addr);
+	}
+	res = test1 & test2;
+	printf("%d\n", res);
+	return addr_len + 1;
+}
+
+#include "cpu/exec/template-end.h"
+
