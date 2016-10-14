d263ccbde3e7439691581dca5408f67c99ac29c5
tracer-ics2016 <tracer@njuics.org>
2016-10-14 16:35:26 +0800

    >  compile NEMU 151242041 hatsuyuki Linux PA 4.4.0-42-generic #62-Ubuntu SMP Fri Oct 7 23:10:22 UTC 2016 i686 i686 i686 GNU/Linux  16:35:26 up  2:34,  1 user,  load average: 0.08, 0.02, 0.01 7e0c36073d077c5c173d67bfd450521d5cbd747f

--- nemu/src/cpu/exec/string/stos-template.h
+++ nemu/src/cpu/exec/string/stos-template.h
@@ -4,7 +4,7 @@
 
 static void do_execute(){
 //	printf("%x\n", reg_l(R_EDI));
-	REG(R_EAX) = swaddr_read(reg_l(R_EDI), DATA_BYTE);
+	swaddr_write(reg_l(R_EDI), DATA_BYTE, REG(R_EAX));
 //	printf("OK\n");
 	if(cpu.DF == 0){
 		reg_l(R_EDI) += DATA_BYTE;
