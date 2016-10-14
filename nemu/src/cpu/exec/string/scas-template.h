89e8baf918bfe0727d31f6a4c02105a0e98092a6
tracer-ics2016 <tracer@njuics.org>
2016-10-14 15:49:20 +0800

    >  compile NEMU 151242041 hatsuyuki Linux PA 4.4.0-42-generic #62-Ubuntu SMP Fri Oct 7 23:10:22 UTC 2016 i686 i686 i686 GNU/Linux  15:49:20 up  1:47,  1 user,  load average: 0.01, 0.01, 0.00 6cc3740348ad794ed31ec6918151f367896a9cf7

--- nemu/src/cpu/exec/string/scas-template.h
+++ nemu/src/cpu/exec/string/scas-template.h
@@ -9,12 +9,14 @@ static void do_execute(){
 	DATA_TYPE res = src - dest;
 	cpu.CF = src < dest;
 	arith_flag(src, MSB(src), (~dest) + 1, MSB((~dest) + 1), res, MSB(res));
+	printf("%x\n", cpu.edi);
 	if(cpu.DF == 0){
 		REG(R_EDI) += DATA_BYTE;
 	}
 	else{
 		REG(R_EDI) -= DATA_BYTE;
 	}
+	printf("%x\n", cpu.edi);
 	print_asm("scas");
 }
 
