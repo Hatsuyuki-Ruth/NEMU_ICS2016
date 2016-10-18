f499bc77984e58c4f990fd3a65c0299952b67094
tracer-ics2016 <tracer@njuics.org>
2016-10-19 01:12:51 +0800

    >  compile kernel 151242041 hatsuyuki Linux PA 4.4.0-42-generic #62-Ubuntu SMP Fri Oct 7 23:10:22 UTC 2016 i686 i686 i686 GNU/Linux  01:12:51 up  3:50,  1 user,  load average: 0.02, 0.04, 0.00 aa07cec3a3f1abc512440aff8e453acba0a13df

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -54,11 +54,15 @@ uint32_t loader() {
 #else
 			ramdisk_read((uint8_t*)ph->p_vaddr, ph->p_offset, ph->p_filesz);
 #endif
-			nemu_assert(0);
+			//nemu_assert(0);
 			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
-
+#ifdef HAS_DEVICE
+			//ide_read((uint
+			//ramdisk_read((uint8_t*)ph->p_vaddr + ph->p_filesz, , 
+#else
+#endif
 
 #ifdef IA32_PAGE
 			/* Record the program break for future use. */
@@ -67,7 +71,7 @@ uint32_t loader() {
 			if(cur_brk < new_brk) { max_brk = cur_brk = new_brk; }
 #endif
 		}
-		ph++;
+		ph += elf->e_phentsize;
 	}
 
 	volatile uint32_t entry = elf->e_entry;
