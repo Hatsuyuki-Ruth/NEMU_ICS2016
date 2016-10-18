8f6bc2dde67651c198c842b8f1d5433aa82e042a
tracer-ics2016 <tracer@njuics.org>
2016-10-19 01:26:18 +0800

    >  compile kernel 151242041 hatsuyuki Linux PA 4.4.0-42-generic #62-Ubuntu SMP Fri Oct 7 23:10:22 UTC 2016 i686 i686 i686 GNU/Linux  01:26:18 up  4:04,  1 user,  load average: 0.00, 0.00, 0.00 11a996f227a9628e3ba812e03099181cae4038bf

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -49,11 +49,11 @@ uint32_t loader() {
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
-			nemu_assert(ph->p_offset + ph->p_filesz < (1 << 27));
+			//nemu_assert(ph->p_offset + ph->p_filesz < (1 << 27));
 #ifdef HAS_DEVICE
 			ide_read((uint8_t*)ph->p_vaddr, ph->p_offset, ph->p_filesz);
 #else
-			ramdisk_read((uint8_t*)ph->p_vaddr, ph->p_offset, ph->p_filesz);
+			//ramdisk_read((uint8_t*)ph->p_vaddr, ph->p_offset, ph->p_filesz);
 #endif
 			//nemu_assert(0);
 			/* TODO: zero the memory region 
