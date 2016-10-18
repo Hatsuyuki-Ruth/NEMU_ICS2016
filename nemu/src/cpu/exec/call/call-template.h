85c706eb47a8d8ccc16ca032f533ad3a8fb22459
tracer-ics2016 <tracer@njuics.org>
2016-10-18 22:52:15 +0800

    >  compile NEMU 151242041 hatsuyuki Linux PA 4.4.0-42-generic #62-Ubuntu SMP Fri Oct 7 23:10:22 UTC 2016 i686 i686 i686 GNU/Linux  22:52:15 up  1:30,  1 user,  load average: 0.06, 0.07, 0.09 259bc4aa9814cb28470fb56a71fd1e96740a2928

--- nemu/src/cpu/exec/call/call-template.h
+++ nemu/src/cpu/exec/call/call-template.h
@@ -5,10 +5,12 @@
 #include "cpu/exec/helper.h"
 #include "cpu/exec/template-start.h"
 
+#define instr call
+
 make_helper(concat(call_i_, SUFFIX)){
 	int im = concat(decode_rm_, SUFFIX)(eip + 1);
 	cpu.esp -= DATA_BYTE;
-	swaddr_write(cpu.esp, 4, cpu.eip + im);
+	swaddr_write(cpu.esp, DATA_BYTE, cpu.eip + im);
 	cpu.eip += op_src->val;
 	print_asm("call 0x%x", cpu.eip + 1 + im);
 	return 0;
@@ -17,7 +19,7 @@ make_helper(concat(call_i_, SUFFIX)){
 make_helper(concat(call_rm_, SUFFIX)){
 	int im = instr_fetch(eip + 1, DATA_BYTE);
 	cpu.esp -= DATA_BYTE;
-	swaddr_write(cpu.esp, 4, cpu.eip + im);
+	swaddr_write(cpu.esp, DATA_BYTE, cpu.eip + im);
 	cpu.eip = cpu.eip + im;
 	print_asm("call 0x%x", eip + im + 5);
 	return DATA_BYTE + 1;
