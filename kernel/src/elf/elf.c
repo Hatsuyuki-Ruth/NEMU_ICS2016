14ae643b619064e76d2c25c9e7e91be9b2e2200f
tracer-ics2016 <tracer@njuics.org>
2016-10-19 01:05:26 +0800

    >  compile kernel 151242041 hatsuyuki Linux PA 4.4.0-42-generic #62-Ubuntu SMP Fri Oct 7 23:10:22 UTC 2016 i686 i686 i686 GNU/Linux  01:05:26 up  3:43,  1 user,  load average: 0.00, 0.02, 0.00 2c0f783b45a40a7f6aa2197c979f473e6d36ee68

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -54,6 +54,7 @@ uint32_t loader() {
 #else
 			ramdisk_read((uint8_t*)ph->p_vaddr, ph->p_offset, ph->p_filesz);
 #endif
+			nemu_assert(0);
 			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
