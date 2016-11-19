f4cb1d5a87f0aef3bca13f9cc490dc2d523f4952
tracer-ics2016 <tracer@njuics.org>
2016-11-19 16:04:25 +0800

    >  compile NEMU 151242041 hatsuyuki Linux PA 4.4.0-45-generic #66-Ubuntu SMP Wed Oct 19 14:12:05 UTC 2016 i686 i686 i686 GNU/Linux  16:04:25 up  1:20,  1 user,  load average: 0.06, 0.02, 0.00 d5207391986c20372177d1ea8c7a164c90a85337

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -13,7 +13,7 @@ uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 	for (j = len - 1; j >= 0; j--) {
 		l1_read(&tmp, addr + j);
 		result = result * 0x100U + tmp;
-		//printf("tmp: 0x%x\n", tmp);
+		printf("tmp: 0x%x\n", tmp);
 	}
 	printf("Addr: 0x%x. Len: %d. Cache result: 0x%x. DRAM result: 0x%x\n", addr, len, result, dram_read(addr, len) & (~0u >> ((4 - len) << 3)));
 	return result;
