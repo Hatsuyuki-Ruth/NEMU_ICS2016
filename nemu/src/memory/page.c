f658eeb22f847dd4584af0db5ed28cdf1d54c9cb
tracer-ics2016 <tracer@njuics.org>
2016-12-08 20:15:29 +0800

    >  compile NEMU 151242041 hatsuyuki Linux PA 4.4.0-45-generic #66-Ubuntu SMP Wed Oct 19 14:12:05 UTC 2016 i686 i686 i686 GNU/Linux  20:15:29 up  3:54,  1 user,  load average: 0.00, 0.00, 0.00 d5cd0d7b2d2a3b1d742c750aa75798f2ab7e2d0a

--- nemu/src/memory/page.c
+++ nemu/src/memory/page.c
@@ -5,6 +5,14 @@
 hwaddr_t page_translate(lnaddr_t addr) {
 	if (!cpu.PG) return addr;
 	else {
-		return addr;
+		uint32_t dir_ind = (addr >> 22) & 0x3ff;
+		uint32_t table_ind = (addr >> 12) & 0x3ff;
+		uint32_t offset = addr & 0xfff;
+		uint32_t dir = cpu.CR3 & 0xfffff000;
+		uint32_t dir_entry = hwaddr_read(dir + (dir_ind << 2), 4);
+		uint32_t table = dir_entry & 0xfffff000;
+		uint32_t table_entry = hwaddr_read(table + (table_ind << 2), 4);
+		return (table_entry & 0xfffff000) | offset;
 	}
 }
+
