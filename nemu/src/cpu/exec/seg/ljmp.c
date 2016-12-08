08c20a2ecf1dad4a023ea4fd59f2b88c93c94226
tracer-ics2016 <tracer@njuics.org>
2016-12-08 19:09:44 +0800

    >  compile NEMU 151242041 hatsuyuki Linux PA 4.4.0-45-generic #66-Ubuntu SMP Wed Oct 19 14:12:05 UTC 2016 i686 i686 i686 GNU/Linux  19:09:44 up  2:48,  1 user,  load average: 0.00, 0.00, 0.00 89aeee07847de24fe7f9ef3e2c576223fac89971

--- nemu/src/cpu/exec/seg/ljmp.c
+++ nemu/src/cpu/exec/seg/ljmp.c
@@ -1,8 +1,8 @@
 #include "cpu/exec/helper.h"
 
 make_helper(ljmp) {
-	uint32_t new_eip = instr_fetch(eip + 1, 4);
-	uint16_t seg = instr_fetch(eip + 1 + 4, 2);
+	uint32_t new_eip = instr_fetch(cpu.eip + 1, 4);
+	uint16_t seg = instr_fetch(cpu.eip + 1 + 4, 2);
 	cpu.eip = new_eip;
 	cpu.seg[R_CS] = seg;
 	print_asm("ljmp 0x%x, 0x%x\n", seg, new_eip);
