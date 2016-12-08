036b3fa8c87efff64dbb61881bd16eba615932a0
tracer-ics2016 <tracer@njuics.org>
2016-12-08 19:16:35 +0800

    >  compile NEMU 151242041 hatsuyuki Linux PA 4.4.0-45-generic #66-Ubuntu SMP Wed Oct 19 14:12:05 UTC 2016 i686 i686 i686 GNU/Linux  19:16:35 up  2:55,  1 user,  load average: 0.00, 0.00, 0.00 af22150d12d1392e60adb3f56116aa6df3407814

--- nemu/src/memory/seg.c
+++ nemu/src/memory/seg.c
@@ -7,7 +7,7 @@ lnaddr_t seg_translate(swaddr_t addr, uint8_t sreg) {
 	uint32_t seg_des1 = hwaddr_read(cpu.GDTR + (cpu.seg[sreg] >> 3), 4);
 	uint32_t seg_des2 = hwaddr_read(cpu.GDTR + (cpu.seg[sreg] >> 3) + 4, 4);
 	uint32_t seg_base = (seg_des1 >> 16) + (seg_des2 & 0xff) + ((seg_des2 >> 24) << 24);
-	printf("%s: %x\n", seg_reg_name[sreg], seg_base);
+	printf("%s: %x + %x\n", seg_reg_name[sreg], seg_base, addr);
 	return seg_base + addr;
 }
 
